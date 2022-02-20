5a816f79ebecd3bbbbe3d0d7eb611530fc91bb18
Alceu Medeiros <3277393+alceu@users.noreply.github.com>
2022-02-20 13:32:40 -0300

    Pre-commit test perform only when have js src files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,16 @@
 module.exports = {
   // linters: {
-  '**/*.js': [ // '**/*.+(js|jsx|ts|graphql|vue)': [ 
-    'npm run lint-fix -- --cache',
-    // 'jest --findRelatedTests',
-    // 'git add',
-  ],
-  '**/*.+(js|md|css|json)': [ // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [ 
+    '**/*.js': [ // '**/*.+(js|jsx|ts|graphql|vue)': [ 
+      'npm run lint-fix -- --cache',
+      // 'jest --findRelatedTests',
+      // 'git add',
+    ],
+    './src/**/*.js': [ // '**/*.+(js|jsx|ts|graphql|vue)': [ 
+      'CI=true npm run test'
+      // 'jest --findRelatedTests',
+      // 'git add',
+    ],
+      '**/*.+(js|md|css|json)': [ // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [ 
     'npm run format', //"./node_modules/.bin/prettier --write './**/*.{js,jsx,ts,tsx,css,md,json}' --config ./.prettierrc.js",
   ],
   // },
