01cbd0b0144373a1a345463c49d5f31c32e0aad7
Alceu Medeiros <3277393+alceu@users.noreply.github.com>
2022-02-21 11:04:36 -0300

    Pre-commit review and bugfixes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,21 @@
 module.exports = {
   // linters: {
-  '**/*.js': [ // '**/*.+(js|jsx|ts|graphql|vue)': [ 
+  './**/*.js': [
+    // '**/*.+(js|jsx|ts|graphql|vue)': [
     'npm run lint-fix -- --cache',
-    // 'jest --findRelatedTests',
-    // 'git add',
+    'git add',
   ],
-  '**/*.+(js|md|css|json)': [ // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [ 
+  './src/**/*.js': [
+    // '**/*.+(js|jsx|ts|graphql|vue)': [
+    'npm run test:staged',
+    // 'jest --findRelatedTests',
+  ],
+  './**/*.+(js|md|css|json)': [
+    // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
     'npm run format', //"./node_modules/.bin/prettier --write './**/*.{js,jsx,ts,tsx,css,md,json}' --config ./.prettierrc.js",
+    'git add',
+    //"*.css": "stylelint",
+    //"*.scss": "stylelint --syntax=scss"
   ],
   // },
 };
