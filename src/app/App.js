ee0a58f88fbaa1d6bcf5b16c868dad95513bdae7
Alceu Medeiros <3277393+alceu@users.noreply.github.com>
2022-03-01 17:50:04 -0300

    App scss file name quickfix

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import './App.module.scss';
+import './App.scss';
 
 export default function App() {
   return (
