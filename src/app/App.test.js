d18d1504fe3d3f2b95567b28c7b24842be5b8705
Alceu Medeiros <3277393+alceu@users.noreply.github.com>
2022-03-03 13:05:59 -0300

    Mock render nested components naming review

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -3,14 +3,14 @@ import { screen } from '@testing-library/react';
 import { render } from 'utils/test';
 import App from './App';
 
-const mockedUsersTextResult = 'MockedUsers';
-jest.mock('features/user/Users', () => () => mockedUsersTextResult);
+const renderUsersMockResult = 'MockedUsers';
+jest.mock('features/user/Users', () => () => renderUsersMockResult);
 
 describe('App base', () => {
   it('renders title and users list feature', async () => {
     render(<App />);
 
     expect(screen.getByRole('heading')).toHaveTextContent('Users posts');
-    expect(screen.getByText(mockedUsersTextResult)).toBeInTheDocument();
+    expect(screen.getByText(renderUsersMockResult)).toBeInTheDocument();
   });
 });
