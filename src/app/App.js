12039b2360ef63d85cde2b2bf92cda754216524a
Alceu Medeiros <3277393+alceu@users.noreply.github.com>
2022-03-09 11:23:49 -0300

    Linting fix code review

--- src/app/App.js
+++ src/app/App.js
@@ -6,8 +6,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import './App.scss';
-
 export default function App() {
   return (
     <Container>
